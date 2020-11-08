90adae8ee966ad1b5b08d036f983c8312fbb48d0
Mainali <abc@abc.abc>
2020-11-08 12:24:21 +0545

    create react app

---
+++ src/component/homePage/index.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+import "./homePage.styles.scss";
+
+class HomePage extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="homepage">
+        <div className="directory-menu">
+          <div className="menu-item">
+            <div className="content">
+              <h1 className="title">HATS</h1>
+              <span className="subtitle">SHOP NOW</span>
+            </div>
+          </div>
+          <div className="menu-item">
+            <div className="content">
+              <h1 className="title">Test</h1>
+              <span className="subtitle">SHOP NOW</span>
+            </div>
+          </div>
+          <div className="menu-item">
+            <div className="content">
+              <h1 className="title">Test</h1>
+              <span className="subtitle">SHOP NOW</span>
+            </div>
+          </div>
+          <div className="menu-item">
+            <div className="content">
+              <h1 className="title">Test</h1>
+              <span className="subtitle">SHOP NOW</span>
+            </div>
+          </div>
+          <div className="menu-item">
+            <div className="content">
+              <h1 className="title">Test</h1>
+              <span className="subtitle">SHOP NOW</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
+
+
+// import React from 'react';
+
+// import './homePage.styles.scss';
+
+// const HomePage = () => (
+//   <div className='homepage'>
+//     <h1>Welcome to my Homepage</h1>
+//     <div className='directory-menu'>
+//       <div className='menu-item'>
+//         <div className='content'>
+//           <div className='title'>HATS</div>
+//           <span className='subtitle'>SHOP NOW</span>
+//         </div>
+//       </div>
+//       <div className='menu-item'>
+//         <div className='content'>
+//           <div className='title'>JACKETS</div>
+//           <span className='subtitle'>SHOP NOW</span>
+//         </div>
+//       </div>
+//       <div className='menu-item'>
+//         <div className='content'>
+//           <div className='title'>SHOES</div>
+//           <span className='subtitle'>SHOP NOW</span>
+//         </div>
+//       </div>
+//       <div className='menu-item'>
+//         <div className='content'>
+//           <div className='title'>WOMENS</div>
+//           <span className='subtitle'>SHOP NOW</span>
+//         </div>
+//       </div>
+//       <div className='menu-item'>
+//         <div className='content'>
+//           <div className='title'>MENS</div>
+//           <span className='subtitle'>SHOP NOW</span>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// );
+
+// export default HomePage;
